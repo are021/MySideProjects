1126ea8e81a254cee2438f67121153612059ad85
are021 <islamare@msu.edu>
2023-03-03 18:30:29 -0500

    Started developing the initial Binary Tree Class and Special Directory Class

---
+++ Python/binary-node.py
@@ -0,0 +1,13 @@
+class FileDirectory:
+    def __init__(self):
+        file_list = []
+        
+
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.right_child = None
+        self.left_child  = None
