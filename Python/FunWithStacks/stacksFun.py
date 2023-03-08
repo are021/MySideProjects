f57305bd691764b5d07ab36171d6350c29899324
are021 <islamare@msu.edu>
2023-03-08 16:04:51 -0500

    Finished the initial game. Just need to somehow add timer functionality

--- Python/FunWithStacks/stacksFun.py
+++ Python/FunWithStacks/stacksFun.py
@@ -1,37 +1,74 @@
 from stack import *
 import random
+import time
+import threading
+
 '''
 Difficulty 1 = Both Single Digit Numbers
 Difficulty 2 = One Double, One Single
 
 '''
 def generateProblems(dif, stack):
-    type = random.randint(1,2)
+    type = random.randint(1,4)
 
     if dif == 1:
-        val1 = random.randint(0,9)
-        val2 = random.randint(0,9)
+        if type == 1 or type == 2:
+            val1 = random.randint(0,9)
+            val2 = random.randint(0,9)
+        elif type == 3:
+            val1 = random.randint(0,6)
+            val2 = random.randint(0,6)
+        else:
+            val1 = random.randint(0,30)
+            val2 = random.randint(1,10)
+
+  
     if dif == 2:
-        val1 = random.randint(0,99)
-        val2 = random.randint(0,99)
+        if type == 1 or type == 2:
+            val1 = random.randint(0,99)
+            val2 = random.randint(0,99)
+        elif type == 3:
+            val1 = random.randint(0,12)
+            val2 = random.randint(0,12)
+        else:
+            val1 = random.randint(0,100)
+            val2 = random.randint(1,30)
     if dif == 3:
-        val1 = random.randint(0,999)
-        val2 = random.randint(0,999)
-
+        if type == 1 or type == 2:
+            val1 = random.randint(0,999)
+            val2 = random.randint(0,999)
+        elif type == 3:
+            val1 = random.randint(0,20)
+            val2 = random.randint(0,20)
+        else:
+            val1 = random.randint(0,200)
+            val2 = random.randint(1,50)
     if type == 1:
-        return ((val1 + val2), "{} + {}".format(val1,val2))
+        return ((val1 + val2), "{} + {}\n".format(val1,val2))
     elif type == 2:
-        return ((val1 - val2), "{} - {}".format(val1,val2))
+        return ((val1 - val2), "{} - {}\n".format(val1,val2))
+    elif type == 3:
+        return ((val1 * val2), "{} * {}\n".format(val1,val2))
+    else:
+        return ((val1 // val2), "{} / {}\n".format(val1,val2))
 
 
-
+def countdown(t):
+    
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    print("Hello")
     
 
 
 
 def startGame(stack):
     try:
-        difficulty = int(input("Select a Difficult /n 1. Easy /n 2. Medium /n 3. Hard"))
+        difficulty = int(input("Select a Difficult \n 1. Easy \n 2. Medium \n 3. Hard \n"))
     except:
         print("Input A Valid Number!")
         return (stack,False,None)
@@ -51,6 +88,17 @@ def startGame(stack):
         stack.push(Dummy())
     return (stack,True,difficulty)
 
+def askQuestion(answer,questionString):
+    try:
+        userPrompt = int(input(questionString))
+        if userPrompt == answer:
+            return True
+        else:
+            return False
+
+    except:
+        print("Wrong Answer")
+        return False
 
 
 def main():
@@ -58,7 +106,19 @@ def main():
     quizStack, untilTrue, difficulty = startGame(quizStack)
     while untilTrue == False:
         quizStack, untilTrue, difficulty = startGame(quizStack)
-
-
+    #t = threading.Thread(target = countdown,args= (15,))
+    while not quizStack.is_empty():
+        #t.start()
+        answer, questionString = generateProblems(difficulty,quizStack)
+        qBool = askQuestion(answer,questionString)
+        if qBool == True:
+            print("Correct! Questions Left = {}".format(len(quizStack)-1))
+            quizStack.pop()
+        elif qBool == False:
+            print("Incorrect! Questions Left = {}".format(len(quizStack)+1))
+            quizStack.push(Dummy())
+        else:
+            break
+    print("YOU WIN")
 if __name__ == "__main__":
     main()
