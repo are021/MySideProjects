99b2a2e8983829e2cc23e384419c587522dd001c
are021 <islamare@msu.edu>
2023-03-10 20:30:30 -0500

    Added some C Projects I've done

---
+++ C_C++/C_Code/ReverseString/README.md
@@ -0,0 +1,3 @@
+# Reversing a String with Memory Allocation
+
+In my Computer Systems class, I was having difficulty understanding memory allocation in  C. Here is do a basic string reversal with memory allocation using Calloc() and Free(). This is a relatively simple project, but it helped me understand C better.
---
+++ C_C++/C_Code/ReverseString/str-reverse.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* reverseString(char* name);
+int main(){
+    char val[50];
+    printf("Enter a string (Max Length): ");
+    scanf("%s", val);
+    char* reversed = reverseString(val);
+    printf("Your name is %s.", reversed);
+    free(reversed);
+    return 0;
+}
+
+
+char* reverseString(char* name){
+    char* reversed = (char*)calloc(strlen(name)+1,sizeof(char));
+    int j = 0;
+    for (int i = strlen(name)-1; i >= 0; i--){
+        reversed[j] = name[i];
+        j++;
+    }
+    return reversed;
+}
