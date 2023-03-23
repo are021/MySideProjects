45ffd7bf825ee09f0269a704b7007a0bb2a1c4c1
are021 <islamare@msu.edu>
2023-03-23 03:08:38 -0400

    Added 3 Panels to the Frame, and implemented a working dropdown menu

--- Java/ProjectPokeSim/src/DexFrame.java
+++ Java/ProjectPokeSim/src/DexFrame.java
@@ -1,34 +1,73 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 
 public class DexFrame extends JFrame implements ActionListener {
 	
 	JLabel label;
 	JComboBox box;
+	JPanel selectionPanel;
+	JPanel imagePanel;
+	JPanel infoPanel;
+	
+	ImageIcon pikachu = new ImageIcon("fixed.png");
 	DexFrame(String[] pkmn){
 		
+		selectionPanel = new JPanel();
+		selectionPanel.setLayout(new FlowLayout(FlowLayout.CENTER,20,30));
+		selectionPanel.setPreferredSize(new Dimension(250,100));
+		label = new JLabel("Select a Pokemon");
+		label.setFont(new Font("Arial",Font.BOLD,18));
 		
+		label.setIcon(pikachu);
+		label.setIconTextGap(50);
+		selectionPanel.add(label);
 		box = new JComboBox(pkmn);
 		box.addActionListener(this);
-		this.add(box);
+		selectionPanel.add(box);
+		
+		
+		this.add(selectionPanel,BorderLayout.NORTH);
+		
+		///////Panel 2//////////
+		JLabel label2 = new JLabel("IS THIS WORKING???");
+		imagePanel = new JPanel();
+		imagePanel.setBackground(Color.LIGHT_GRAY);
+		imagePanel.setPreferredSize(new Dimension(250,250));
+		imagePanel.add(label2);
+		
+		this.add(imagePanel,BorderLayout.CENTER);
+		
+		
+		////////Panel3///////////
+		infoPanel = new JPanel();
+		infoPanel.setBackground(Color.gray);
+		infoPanel.setPreferredSize(new Dimension(250,350));
+		
+		this.add(infoPanel,BorderLayout.SOUTH);
+		
 		this.setTitle("My PokeDex");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// This color is fire lowkey 120,50,255
 		this.getContentPane().setBackground(new Color(179, 196, 196));
-		this.setLayout(new FlowLayout());
-		this.pack();
+		this.setSize(800,1200);
+		//this.setLayout();
+		//this.pack();
 		this.setVisible(true);
 
 		ImageIcon image = new ImageIcon("IMG_4624.jpg");
